cf93a844d2cf3456bcd2eb6bd6aa5ad93fb420c7
JaeY0ungJung <hanol5216@gmail.com>
2023-07-08 21:51:20 +0900

    크롤링으로 생성된 csv 파일, xlsx 파일 업르드

--- csv_to_excel.py
+++ csv_to_excel.py
@@ -6,10 +6,13 @@ def csv_to_excel(area):
     csvReader = pd.read_csv(csvfile)
 
     # 저장할 xlsx파일의 이름을 정함
-    save_xlsx = pd.ExcelWriter(f"./excel/{area}.xlsx")
+    save_xlsx = pd.ExcelWriter(f"./excel/{area}술집.xlsx")
     csvReader.to_excel(save_xlsx, index = False)
 
     #xlsx 파일로 저장
     save_xlsx.save()
 
 csv_to_excel("홍대")
+csv_to_excel("일산")
+csv_to_excel("강남")
+csv_to_excel("신촌")
