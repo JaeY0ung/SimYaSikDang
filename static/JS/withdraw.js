b036d82cf2ee2b9e853196f2bbf2267be3a0225a
JaeY0ungJung <hanol5216@gmail.com>
2023-10-15 00:24:08 +0900

    feat: 회원탈퇴 프로트, 백 기능 구현

---
+++ static/JS/withdraw.js
@@ -0,0 +1,37 @@
+const withdrawButton = document.querySelector('#withdraw-button');
+const withdrawButtonModal = document.querySelector('#withdraw-button-modal');
+const modal = document.querySelector("#modal");
+const closeBtn = modal.querySelector(".btn-close");
+
+withdrawButton.addEventListener("click", (e) => {
+    modal.style.display = "flex";
+})
+
+closeBtn.addEventListener("click", (e) => {
+    modal.style.display = "none";
+})
+
+modal.addEventListener("click", e => {
+    const evTarget = e.target
+    if(evTarget.classList.contains("modal-overlay")) {
+        modal.style.display = "none"
+    }
+})
+
+withdrawButtonModal.addEventListener('click' , (e) => {
+    fetch('/withdraw', {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify({
+            title: "Delete User",
+            body: "Delete User",
+        })
+    })
+    .then((response) => {
+        location.replace('/');
+        console.log("response:", response);
+    })
+    .then((error) => console.log("error:", error))
+});
